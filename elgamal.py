e1793019297d7f2f2ca9f7b09379e56427a083e7
willdunklin <will.dunklin@kitware.com>
2023-03-05 13:41:31 -0500

    Update elgamal.py with better types

--- elgamal.py
+++ elgamal.py
@@ -44,25 +44,25 @@ class ElGamal:
         x = (x * f_inv) % self.p
         return x
 
-    def encrypt_text(self, text: str) -> list[tuple[int]]:
-        b = text.encode('utf-8') # byte string representing text
+    def encrypt_text(self, text: str) -> list[tuple[int, int]]:
+        bytes = text.encode('utf-8') # byte string representing text
         bytes_per_n = max(self.k // 8, 1)
 
         # split text into chunks of k bits and convert them to integers
         ints = []
-        for i in range(math.ceil(len(b) / bytes_per_n)):
-            byte_substr = b[i*bytes_per_n : (i+1)*bytes_per_n]
+        for i in range(math.ceil(len(bytes) / bytes_per_n)):
+            byte_substr = bytes[i*bytes_per_n : (i+1)*bytes_per_n]
             ints.append(int(byte_substr.hex(), base=16))
 
         # encrypt the integer representation of text
         return [self.encrypt(i) for i in ints]
 
-    def decrypt_text(self, ints: list[tuple[int]]) -> str:
+    def decrypt_text(self, cypher: list[tuple[int, int]]) -> str:
         text = ''
 
-        for (x, b) in ints:
-            x = self.decrypt(x, b)
-            num_bytes = math.ceil(math.log(x)/math.log(256)) if x != 1 else 1
-            text += x.to_bytes(num_bytes, 'big').decode('utf-8')
+        for (x, beta) in cypher:
+            m = self.decrypt(x, beta)
+            num_bytes = 1 if m == 1 else math.ceil(math.log(x)/math.log(256))
+            text += m.to_bytes(num_bytes, 'big').decode('utf-8')
 
         return text
